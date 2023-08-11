50bd564ec1c02751f6bc1b024d118032c5109118
themrsung <themrsung@gmail.com>
2023-08-11 20:29:13 +0900

    Back to Giza

--- src/main/java/civitas/celestis/Moebius.java
+++ src/main/java/civitas/celestis/Moebius.java
@@ -1,15 +1,7 @@
 package civitas.celestis;
 
-import civitas.celestis.event.lifecycle.EventManager;
-import civitas.celestis.event.lifecycle.SyncEventManager;
-import civitas.celestis.listener.object.ObjectsCollidedListener;
-import civitas.celestis.task.lifecycle.AsyncScheduler;
-import civitas.celestis.task.lifecycle.Scheduler;
-import civitas.celestis.world.lifecycle.SyncWorldManager;
-import civitas.celestis.world.lifecycle.WorldManager;
 import jakarta.annotation.Nonnull;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -24,13 +16,7 @@ public final class Moebius {
      * Starts the Moebius engine.
      */
     public static void start() {
-        // Register listeners
-        registerListeners();
-
         // Start modules
-        scheduler.start();
-        eventManager.start();
-        worldManager.start();
     }
 
     /**
@@ -38,44 +24,11 @@ public final class Moebius {
      */
     public static void stop() {
         // Stop modules
-        scheduler.stop();
-        eventManager.stop();
-        worldManager.stop();
     }
 
     //
     // Getters
     //
-
-    /**
-     * Gets the scheduler instance of Moebius.
-     *
-     * @return Scheduler instance
-     */
-    @Nonnull
-    public static Scheduler getScheduler() {
-        return scheduler;
-    }
-
-    /**
-     * Gets the event manager instance of Moebius.
-     *
-     * @return Event manager instance
-     */
-    @Nonnull
-    public static EventManager getEventManager() {
-        return eventManager;
-    }
-
-    /**
-     * Gets the world manager instance of Moebius.
-     *
-     * @return World manager instance
-     */
-    @Nonnull
-    public static WorldManager getWorldManager() {
-        return worldManager;
-    }
 
     /**
      * Gets the logger instance of Moebius.
@@ -90,22 +43,5 @@ public final class Moebius {
     //
     // Definitions
     //
-
-    private static final Scheduler scheduler = new AsyncScheduler();
-    private static final EventManager eventManager = new SyncEventManager();
-    private static final WorldManager worldManager = new SyncWorldManager();
     private static final Logger logger = Logger.getLogger("Moebius");
-
-    //
-    // Internal
-    //
-
-    /**
-     * Registers first-party listeners to the event manager.
-     */
-    private static void registerListeners() {
-        eventManager.register(List.of(
-                new ObjectsCollidedListener()
-        ));
-    }
 }
--- src/main/java/civitas/celestis/event/Event.java
+++
@@ -1,9 +0,0 @@
-package civitas.celestis.event;
-
-/**
- * A marker interface for classes which can be called to an event manager.
- *
- * @see Listener
- */
-public interface Event {
-}
